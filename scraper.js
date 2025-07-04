d9f34cee1417164d56a6aaf5c92fd803af973ed7
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2025-07-04 14:35:58 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true
+});
+
   const page = await browser.newPage();
 
   // Set viewport and timeout
